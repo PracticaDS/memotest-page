7e1e17ef699be9cc94f2c6606c58f41f4da60512
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-04 22:42:48 +0000

    Deploy PracticaDS/memotest-page to github.com/PracticaDS/memotest-page.git:gh-pages

---
+++ precache-manifest.c7c7af5ed289514ae95920e99dd9bcde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6248d3772d2ae376efb3",
+    "url": "/memotest-page/static/css/main.998c0c10.chunk.css"
+  },
+  {
+    "revision": "6248d3772d2ae376efb3",
+    "url": "/memotest-page/static/js/main.811aea98.chunk.js"
+  },
+  {
+    "revision": "5c5d9fff2afe60122506",
+    "url": "/memotest-page/static/js/runtime~main.eb3fa8d1.js"
+  },
+  {
+    "revision": "3b1e1d6719ffac6362a5",
+    "url": "/memotest-page/static/js/2.759a60b2.chunk.js"
+  },
+  {
+    "revision": "84fbf11ebcc3d5137df9cf05784eeb6b",
+    "url": "/memotest-page/index.html"
+  }
+];
